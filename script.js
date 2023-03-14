b26b73a99bbd77b4197311eadf69d8110a40ca6a
Cristina Joelle <123904661+CristinaJoelle@users.noreply.github.com>
2023-03-13 22:16:56 -0400

    Updated README and fixed bugs relating to the password being generated

--- script.js
+++ script.js
@@ -10,55 +10,54 @@ var symbols = "!@#$%^&*()";
 // References to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Hey! You clicked the button!");
-  //1: Prompt the user for the password criteria.
+generateBtn.addEventListener("click", writePassword);
 
-  //  a. Password length between 8<128.
-  //  b. Ask what characters they want to include.
-  //2. Validate the input.
-  //3. Generate password.
-
-  //4. Display the generated password.
-  return "Generated password will go here!";
-}
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+
+    passwordText.value = newPassword;
+  }
 }
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomIndex];
+  }
+
+  return password;
+}
+
 function getPrompts() {
-  choiceArray = [];
+  choiceArray = "";
 
   characterLength = parseInt(
     prompt("How many characters would you like your password to be?")
   );
   if (characterLength < 8 || characterLength > 128) {
     alert("Please enter a number between 8 and 128.");
-    return getPrompts();
   }
   if (
     confirm("Would you like to include lowercase letters in your password?")
   ) {
-    choiceArray = choiceArray.concat(lowerCase);
+    choiceArray += lowerCase;
   }
   if (
     confirm("Would you like to include uppercase letters in your password?")
   ) {
-    choiceArray = choiceArray.concat(upperCase);
+    choiceArray += upperCase;
   }
   if (confirm("Would you like to include numbers in your password?")) {
-    choiceArray = choiceArray.concat(numbers);
+    choiceArray += numbers;
   }
   if (confirm("Would you like to include symbols in your password?")) {
-    choiceArray = choiceArray.concat(symbols);
+    choiceArray += symbols;
   }
 
   return true;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
